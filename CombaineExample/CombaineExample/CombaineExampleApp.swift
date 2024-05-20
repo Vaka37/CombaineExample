d8bba1c1298718769fa22f4e1ec85b03eb815891
Vaka37 <vak-forever@inbox.ru>
2024-05-20 19:53:33 +0300

    app init

---
+++ CombaineExample/CombaineExample/CombaineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombaineExampleApp.swift
+//  CombaineExample
+//
+//  Created by Vakil on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombaineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
