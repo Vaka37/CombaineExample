1be951ac15d8d7e45c09ba2f6ab1b0025ff07380
Vaka37 <vak-forever@inbox.ru>
2024-05-21 00:12:35 +0300

    Форма валидации

--- CombaineExample/CombaineExample/ContentViewModel.swift
+++ CombaineExample/CombaineExample/ContentViewModel.swift
@@ -4,32 +4,26 @@
 //
 //  Created by Vakil on 20.05.2024.
 //
+///"✅"
+///"🚫"
 
 import Foundation
 import Combine
-import UIKit
 
-extension Notification.Name {
-    static let event = Notification.Name("signal")
+final class ContentViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var validation = ""
+    @Published var password = ""
+    @Published var validationPassword = ""
+    
+    init() {
+        $name
+            .map { $0.isEmpty ? "🚫" : "✅" }
+            .assign(to: &$validation)
+        
+        $password
+            .map { $0.isEmpty ? "🚫" : "✅" }
+            .assign(to: &$validationPassword)
+
+    }
 }
-
-struct People {
-    let name: String
-}
-
-let label = UILabel()
-let namePublisher = NotificationCenter
-    .Publisher(center: .default, name: .event)
-    .map{($0.object as? People)?.name}
-
-let nameSubscruiber = Subscribers
-    .Assign(object: label, keyPath: \.text)
-
-//namePublisher.subscribe(nameSubscruiber)
-
-let people = People(name: "Vaka")
-
-//NotificationCenter.default.post(name: .event, object: people)
-//print(String(describing: label.text))
-
-
