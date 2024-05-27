a2f6664282e07b2b95234ffc51931a6eb1220de8
Vaka37 <vak-forever@inbox.ru>
2024-05-27 10:23:26 +0300

    выполнил 11 задачу

---
+++ CombaineExample/CombaineExample/Model.swift
@@ -0,0 +1,39 @@
+//
+//  Model.swift
+//  CombaineExample
+//
+//  Created by Vakil on 27.05.2024.
+//
+
+import Foundation
+
+struct RickMortyEpisode: Decodable {
+    let results: [Episode]
+}
+
+struct Episode: Decodable {
+    let id: Int
+    let name: String
+    let airDate: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case airDate = "air_date"
+        case episode, characters, url, created
+    }
+}
+
+struct RickMortyCharacters: Decodable {
+    let results: [Characters]
+}
+
+struct Characters: Decodable {
+    let id: Int
+    let name: String
+    let image: String
+    let episode: [String]
+}
