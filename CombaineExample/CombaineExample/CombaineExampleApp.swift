29f8dbad724c2c026801a50e8e2a41252d9b79dc
Vaka37 <vak-forever@inbox.ru>
2024-05-27 09:07:22 +0300

    выполнил 10 задачу

--- CombaineExample/CombaineExample/CombaineExampleApp.swift
+++ CombaineExample/CombaineExample/CombaineExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CombaineExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProductLoadingView()
         }
     }
 }
